84e0c1aed1df310f4f167c3c7af560c006a8fb75
Ishan Hemmige <ishan.hemmige@gmail.com>
2024-02-24 14:55:53 -0500

    add basic endpoints for getting GP

--- databases/gp.py
+++ databases/gp.py
@@ -6,8 +6,23 @@ key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 def add_gp(new_data):
-  try:
-    data = supabase.table("General Practitioner").insert(new_data).execute()
-    return data.data
-  except:
+  if check_exists(new_data["fname"], new_data["lname"]):
     return []
+  data = supabase.table("General Practitioner").insert(new_data).execute()
+  return data.data[0] if data else []
+
+def get_gp():
+  data = supabase.table("General Practitioner").select("*").execute()
+  return data.data if data else []
+
+def get_gp_by_id(path_id):
+  data = supabase.table("General Practitioner").select("*").eq("id", path_id).execute()
+  return data.data[0] if data.data else {}
+
+# Helper functions
+def check_exists(fname, lname):
+    try:
+      data = supabase.table("General Practitioner").select("*").eq("fname", fname).eq("lname", lname).execute()
+      return len(data.data) > 0
+    except:
+      return False  
--- resources/gp.py
+++ resources/gp.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from flask import make_response
-from databases.gp import add_gp
+from databases.gp import add_gp, get_gp, get_gp_by_id
 from schemas import GPSchema
 
 blp = Blueprint("general practitioners", __name__, description="Operations on general practitioners")
@@ -14,6 +14,22 @@ class Submission(MethodView):
     result = add_gp(new_data)
     if not result:
       abort(400, message = "General Practitioner already exists")
+    response = make_response(result)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+  @blp.response(200, GPSchema(many=True))
+  def get(self):
+    result = get_gp()
+    response = make_response(result)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+  
+@blp.route("/gp/<int:gp_id>")
+class GPById(MethodView):
+  @blp.response(200, GPSchema)
+  def get(self, gp_id):
+    result = get_gp_by_id(gp_id)
     response = make_response(result)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
