f61463396a6e299ff5d80e7270328b9d664bf716
Ishan Hemmige <ishan.hemmige@gmail.com>
2024-02-24 14:14:17 -0500

    added endpoint to get submissions from a particular operator

--- databases/submission.py
+++ databases/submission.py
@@ -5,6 +5,13 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
+def add_submission(data):
+  try:
+    data = supabase.table("Submission").insert(data).execute()
+    return data.data
+  except:
+    return []
+
 def get_submissions():
   try:
     data = supabase.table("Submission").select("*").execute()
@@ -13,25 +20,10 @@ def get_submissions():
     return []
 
 
-
 # get the submissions taken by a specific operator
-def get_submission(fname,lname):
-  # get the operator id
+def get_submission(id):
   try:
-    data = supabase.table("Operator").select("*").eq("fname", fname).eq("lname", lname).execute()
-    operator_id = data.data[0]["id"]
-  except:
-    return []
-  # get the submissions taken by the operator
-  try:
-    data = supabase.table("Submission").select("*").eq("operator_id", operator_id).execute()
+    data = supabase.table("Submission").select("*").eq('operator_id', id).execute()
     return data.data
   except:
     return []
-
-
-
-
-# def 
-  
-
--- resources/submission.py
+++ resources/submission.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from flask import make_response
-from databases.submission import get_submissions, get_submission
+from databases.submission import get_submissions, get_submission, add_submission
 from schemas import SubmissionSchema
 
 blp = Blueprint("submissions", __name__, description="Operations on submissions")
@@ -15,12 +15,21 @@ class Submission(MethodView):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
   
-
-@blp.route("/submission/<string:fname>/<string:lname>")
+  @blp.arguments(SubmissionSchema)
+  @blp.response(201, SubmissionSchema)
+  def post(self, new_data):
+    result = add_submission(new_data)
+    if not result:
+      abort(400, message = "Failed to add submission")
+    response = make_response(result)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+  
+@blp.route("/submission/<int:id>")
 class Submission(MethodView):
   @blp.response(200, SubmissionSchema(many=True))
-  def get(self, fname, lname):
-    result = get_submission(fname,lname)
+  def get(self, id):
+    result = get_submission(id)
     response = make_response(result)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
