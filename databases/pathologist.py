86c3dea3912959f6abedef6d5cc69d1375319bf1
Ishan Hemmige <ishan.hemmige@gmail.com>
2024-02-24 14:37:01 -0500

    modified pathologist functions

--- databases/pathologist.py
+++ databases/pathologist.py
@@ -8,25 +8,16 @@ supabase: Client = create_client(url, key)
 def add_path(new_data):
   if check_exists(new_data["fname"], new_data["lname"]):
     return []
-  try:
-    data = supabase.table("Pathologist").insert(new_data).execute()
-    return data.data
-  except:
-    return []
+  data = supabase.table("Pathologist").insert({new_data}).execute()
+  return data.data[0] if data else []
 
 def get_pathologists():
-  try:
-    data = supabase.table("Pathologist").select("*").execute()
-    return data.data
-  except:
-    return []
+  data = supabase.table("Pathologist").select("*").execute()
+  return data.data if data else []
 
 def get_pathologist_by_id(path_id):
-  try:
-    data = supabase.table("Pathologist").select("*").eq("id", path_id).execute()
-    return data.data[0]
-  except:
-    return {}
+  data = supabase.table("Pathologist").select("*").eq("id", path_id).execute()
+  return data.data[0] if data.data else {}
   
 # Helper functions
 def check_exists(fname, lname):
