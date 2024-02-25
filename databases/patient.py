149317606e84871a326282640dc6f7a770d519d0
Ishan Hemmige <ishan.hemmige@gmail.com>
2024-02-25 12:11:38 -0500

    prevent same patient from being added twice

--- databases/patient.py
+++ databases/patient.py
@@ -5,9 +5,12 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-def add_patient(data):
+def add_patient(new_data):
+  data = supabase.table("Patient").select("*").eq("fname", new_data["fname"]).eq("lname", new_data["lname"]).eq("sex", new_data["sex"]).eq("birthdate", new_data["birthdate"]).execute()
+  if data.data:
+    return []
   try:
-    data = supabase.table("Patient").insert(data).execute()
+    data = supabase.table("Patient").insert(new_data).execute()
     return data.data[0]
   except:
     return None
