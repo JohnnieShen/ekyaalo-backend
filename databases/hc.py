e80cb7f05faddcdba6c2d4679c44408725bde575
Ishan Hemmige <ishan.hemmige@gmail.com>
2024-02-25 12:04:43 -0500

    prevent health center duplicates

--- databases/hc.py
+++ databases/hc.py
@@ -6,6 +6,9 @@ key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 def add_hc(new_data):
+  data = supabase.table("Health Center").select("*").eq("name", new_data["name"]).eq("county", new_data["county"]).execute()
+  if data.data:
+    return []
   try:
     data = supabase.table("Health Center").insert(new_data).execute()
     return data.data
